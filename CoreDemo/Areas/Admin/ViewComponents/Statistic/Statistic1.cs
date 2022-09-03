968498a3abbe1a6613383fed8345ef95d096bdb8
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-09-04 01:52:59 +0300

    add project files

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -20,11 +20,8 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v2 = context.Contacts.Count();
             ViewBag.v3 = context.Comments.Count();
 
-<<<<<<< HEAD
             string api = "your api key";
-=======
-            string api = "your apikey";
->>>>>>> 9e72e3430e92e0f24a41f5e85c37dba87f74ceac
+
             string connection = "https://api.openweathermap.org/data/2.5/weather?q=%C4%B0stanbul&mode=xml&lang=tr&units=metric&appid=" + api;
             XDocument document = XDocument.Load(connection);
             ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
